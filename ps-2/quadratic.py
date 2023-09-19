a2ad49a55a79319afc4a4026ce34d10be25e2e41
ama10285 <143143707+ama10285@users.noreply.github.com>
2023-09-18 23:52:36 -0400

    Add files via upload

---
+++ ps-2/quadratic.py
@@ -0,0 +1,31 @@
+from math import *
+import cmath as cm
+def quadratic(a, b, c):
+    d = b**2 - 4*a*c
+    if d == 0:
+        x1_a = -b / (2*a)  
+        x2_a = x1_a  
+        x1_b = -2*c / b  
+        x2_b = x1_b  
+    elif d > 0:
+        x1_a = (-b + sqrt(d)) / (2*a)
+        x2_a = (-b - sqrt(d)) / (2*a)
+        x1_b = -2*c / (b + sqrt(d))
+        x2_b = -2*c / (b - sqrt(d))
+    else:
+        x1_a = (-b + cm.sqrt(d)) / (2*a)
+        x2_a = (-b - cm.sqrt(d)) / (2*a)
+        x1_b = -2*c / (b + cm.sqrt(d))
+        x2_b = -2*c / (b - cm.sqrt(d))
+
+    diff_a = abs(x1_a - x2_a)
+    diff_b = abs(x1_b - x2_b)
+
+    if diff_a < diff_b:
+        return (x1_a, x2_a)
+    else:
+        return (x1_b, x2_b)
+
+a = 0.001 ; b = 1000 ; c = 0.001
+x1,x2= quadratic(a,b,c)
+print("The roots obtained using the more accurate method:","x1=",x1,"x2=",x2)
